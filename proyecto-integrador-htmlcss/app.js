21bc1f39118725d077986e15b3af119680fc1216
ivana <ivana.kruk@gmail.com>
2021-06-15 00:07:29 -0300

    integr

--- proyecto-integrador-htmlcss/app.js
+++ proyecto-integrador-htmlcss/app.js
@@ -1,5 +1,5 @@
 addEventListener('DOMContentLoaded', () =>{
-    const btn_menu = document.querySelector('.btn-menu')
+    const btn_menu = document.querySelector('.btn_menu')
     if (btn_menu){
         btn_menu.addEventListener('click', () => {
             const btn_menu = document.querySelector('.menu_items')
